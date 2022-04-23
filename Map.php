c0ced0f1086ffc4234a69c8bb3d6ca743efc48e1
masahiro96848 <feetwcpic9436@gmail.com>
2022-04-23 13:39:16 +0900

    csvを追加。Map.phpを追加。マッピングする情報を追加

---
+++ Map.php
@@ -0,0 +1,27 @@
+<?php
+
+class Map 
+{
+    public $valueMap;
+
+    public function __construct()
+    {
+        $this->valueMap = valueMapping();
+    }
+
+    public function valueMapping() :array
+    {
+        return [
+            //　ジャンル
+            'genre' => 1,
+            // 名前
+            'name' => 2,
+            // 内容
+            'content' => 3
+        ];
+    }
+
+}
+
+
+?>
